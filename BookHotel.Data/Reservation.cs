3a1bee44387f5a51bac09c962b5f808f6e90ed21
BobTerwilliger <jnickd09@gmail.com>
2020-08-24 09:51:28 -0400

    User reg/login works, Rsv added to data

---
+++ BookHotel.Data/Reservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHotel.Data
+{
+    public class Reservation
+    {
+        [Key]
+        public Guid ReservationId { get; set; }
+        [Required]
+        public int ConfirmationNumber { get; set; }//range and min max length 
+        public string InnCode { get; set; }//***FK***
+        public decimal Rate { get; set; } //range and min max length 
+        public string UserID { get; set; } //user booking rooms
+        public DateTime ArrivialDate { get; set; }
+        public int NumberOfNights { get; set; }
+        public int NumberOfRooms { get; set; } //set to enum ***FK***
+
+    }
+}
