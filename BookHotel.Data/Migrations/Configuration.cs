fd70b231162b9b7fbc1b772bf4531119236c06e5
BobTerwilliger <jnickd09@gmail.com>
2020-09-04 09:42:50 -0400

    Seed data working

--- BookHotel.Data/Migrations/Configuration.cs
+++ BookHotel.Data/Migrations/Configuration.cs
@@ -12,23 +12,23 @@
     internal sealed class Configuration : DbMigrationsConfiguration<BookHotel.Data.ApplicationDbContext>
     {
 
-        /*public class Configuration()
+        public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-        }*/
+        }
 
         protected override void Seed(BookHotel.Data.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
-
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
 
             context.Inncodes.AddOrUpdate(x => x.HotelInnCode,
-                new InnCode { HotelInnCode = "INDMS", HotelName = "Hooiser House on Main", HotelAddress = "101 Main Street", HotelPhoneNumber = 317 - 555 - 6789, HasSpa = false, HasGolfCourse = false, HasRooftopBar = true, NumberOfStars = 3 },
-                new InnCode { HotelInnCode = "INDCD", HotelName = "Carmel Delights Resort and Spa", HotelAddress = "479 Carmel Drive", HotelPhoneNumber = 317 - 555 - 5678, HasSpa = true, HasGolfCourse = true, HasRooftopBar = true, NumberOfStars = 5 },
-                new InnCode { HotelInnCode = "INDAP", HotelName = "Indianapolis Inn at the Airport", HotelAddress = "324 Wier Drive", HotelPhoneNumber = 317 - 555 - 2345, HasSpa = false, HasGolfCourse = false, HasRooftopBar = false, NumberOfStars = 2 },
-                new InnCode { HotelInnCode = "INDFQ", HotelName = "Fishers' Quarters", HotelAddress = "871 116th Street", HotelPhoneNumber = 317 - 555 - 0782, HasSpa = true, HasGolfCourse = false, HasRooftopBar = true, NumberOfStars = 4 }
+                new InnCode { HotelInnCode = "INDMS", HotelName = "Hooiser House on Main", HotelAddress = "101 Main Street", HotelPhoneNumber = 5556789, HasSpa = false, HasGolfCourse = false, HasRooftopBar = true, NumberOfStars = 3 },
+                new InnCode { HotelInnCode = "INDCD", HotelName = "Carmel Delights Resort and Spa", HotelAddress = "479 Carmel Drive", HotelPhoneNumber = 5555678, HasSpa = true, HasGolfCourse = true, HasRooftopBar = true, NumberOfStars = 5 },
+                new InnCode { HotelInnCode = "INDAP", HotelName = "Indianapolis Inn at the Airport", HotelAddress = "324 Wier Drive", HotelPhoneNumber = 5552345, HasSpa = false, HasGolfCourse = false, HasRooftopBar = false, NumberOfStars = 2 },
+                new InnCode { HotelInnCode = "INDFQ", HotelName = "Fishers' Quarters", HotelAddress = "871 116th Street", HotelPhoneNumber = 5550782, HasSpa = true, HasGolfCourse = false, HasRooftopBar = true, NumberOfStars = 4 },
+                new InnCode { HotelInnCode = "INDZS", HotelName = "Zionsville Manor at Cemetery Creek", HotelAddress = "800 Cemetery Trace", HotelPhoneNumber = 5556549, HasSpa = true, HasGolfCourse = true, HasRooftopBar = true, NumberOfStars = 5}
                         );
 
             /*context.Reservations.AddOrUpdate(x => x.ReservationId,
